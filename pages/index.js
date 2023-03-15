be04f90f89a9ede1b7e7c71b058ff36ad7c656e1
Nyelwa <nyelwa@localhost.localdomain>
2023-03-15 19:06:11 +0300

    15th march Another design

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,38 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
-
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>File Converter</title>
+        <title>Just Convert</title>
         <meta name="description" content="Created by Markson F. Senguji" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
         <div className={styles.header}>
-          <h1>Convert Your</h1>
-          <h2>Files Easily</h2>
+          <h1>Just Convert</h1>
+          <h4>
+            Just Convert is an online file converter tool. To get started <br />{" "}
+            use the button below and select files to convert from your <br />{" "}
+            computer
+          </h4>
         </div>
-        <div className={styles.body}>
-          <div className={styles.dotted}>
-            <i className='bi bi-cloud-arrow-down-fill' style={{ color: 'rgb(var(--secondary-color))', fontSize: '1.5rem' }}></i>
-            <h5>Drag&Drop file here</h5>
-            <h5>OR</h5>
-            <button type="" className={styles.button}> <i class="bi bi-paperclip" style={{ color: 'rgb(var(--background-end-rgb))' }}></i> Choose file</button>
-          </div>
+        <div className={styles.rowSelect}>
+          <select>
+            <option>PDF</option>
+          </select>
+          <h2>to</h2>
+          <select>
+            <option>DOCX</option>
+          </select>
         </div>
-        <div className={styles.footer}>
+        <button type="" className={styles.button}> <i className="bi bi-paperclip" style={{ color: 'rgb(var(--background-end-rgb))' }}></i> Choose file</button>
+        {/* <div className={styles.footer}>
           <h5>&copy; Markson F. Senguji | 2023</h5>
-        </div>
+        </div> */}
       </main>
     </>
-  )
+  );
 }
